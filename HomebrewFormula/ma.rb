f2ce0e394ce36ce1a6f87b41704b69031a1092e2
d4k1 <dd41kk@gmail.com>
2026-03-17 15:32:19 +0900

    chore(brew): update formula to v0.1.1

--- HomebrewFormula/ma.rb
+++ HomebrewFormula/ma.rb
@@ -1,13 +1,13 @@
 class Ma < Formula
   desc "TODO: describe your app"
   homepage "https://github.com/daaa1k/ma"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/daaa1k/ma/releases/download/v#{version}/ma-macos-aarch64"
-      sha256 "" # macos-aarch64
+      sha256 "f9d4c0e66b81c71299c1289b22877c7829f596ae803a937c084dc815400390e0" # macos-aarch64
 
       def install
         bin.install "ma-macos-aarch64" => "ma"
@@ -24,7 +24,7 @@ class Ma < Formula
 
   on_linux do
     url "https://github.com/daaa1k/ma/releases/download/v#{version}/ma-linux-x86_64"
-    sha256 "" # linux-x86_64
+    sha256 "32a3f7fe256594abedeb8b772f32e2a82e30d5286c1dfbc09efc70e980447523" # linux-x86_64
 
     def install
       bin.install "ma-linux-x86_64" => "ma"
