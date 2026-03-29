0f215692d4e3f841368ced6f434d1d8ca4a4fd1d
d4k1 <dd41kk@gmail.com>
2026-03-29 20:08:19 +0900

    ci: fix nix binary hash update process

--- flake.nix
+++ flake.nix
@@ -16,8 +16,8 @@
       #   nix store prefetch-file --hash-type sha256 --json <url>
       # Leave as "" to disable the ma-bin package for that platform.
       binaryHashes = {
-        "x86_64-linux"   = "";
-        "aarch64-darwin" = "";
+        "x86_64-linux"   = "sha256-HNTQl4pe1R0tlUaJn1nVUIPj09ZErvKmjfoOShxs2/8=";
+        "aarch64-darwin" = "sha256-9A2ygWwlAs7fwzulzg21XrXWc0lF/1kkv+HPGQHbe1c=";
       };
 
       # Map Nix system strings to GitHub Release artifact names.
